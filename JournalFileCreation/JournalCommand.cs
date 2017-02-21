2a40f0cf165ae888f599ac7407370564b89f8242
umeshKumarPts <umesh.kumar@prototechsolutions.com>
2017-02-21 17:13:53 +0530

    Commite for test

---
+++ JournalFileCreation/JournalCommand.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalFileCreation
+{
+    class JournalCommand
+    {
+        public string[] path = null;
+        public String fileName = "";
+        public String outPutDir = "";
+        public String view = "3D View: {3D}";
+
+        public JournalCommand(string[] fileNamepath, String outDir)
+        {
+            
+            path = fileNamepath;
+            outPutDir = outDir;
+            view = "3D View: {3D}";
+        }
+
+        public List<String> GetCommand()
+        {
+            List<String> list = new List<string>();
+            String f1 = CreateStartCommand();
+            list.Add(f1);
+            foreach (string item in path)
+            {
+                int x = item.LastIndexOf(@"\");
+                String fileName = item.Substring(x + 1, item.Length - x - 1);
+                String outPath = outPutDir + @"\" + fileName;
+                String f2 = CreateCommandForRVTfileOpening(item);
+               String f3 = CreateRuntheExternalCommand("-");
+                String f4 = CreateRvtSaveAsCommand(outPath);
+               // String f4 = CreateRvtSaveCommand();
+                String f5 = CreateRvtCloseDocumentCommand(fileName, view);
+               String command = f2 + f3 +f4 + f5;
+               list.Add(command);
+            }
+            String f6 = CreateRvtCloseCommand();
+            list.Add(f6);
+            return list;
+        }
+        String CreateStartCommand()
+        {
+            /*
+             * 'Autamtion for opening and close the .rvt files and run the external command the revit will close.
+             * 'make sure that addin file aND JOURNAL FILE SHOULD BE SAME DIR 
+             *  Dim Jrn
+             * Set Jrn = CrsJournalScript
+             */
+            StringBuilder sb = new StringBuilder();
+            sb.Append("'***\n'***\n\n");
+            sb.Append("Dim Jrn \nSet Jrn = CrsJournalScript\n");
+            return sb.ToString();
+        }
+
+        String CreateCommandForRVTfileOpening(String filePath)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("'*********************************************************************************\n");
+            sb.Append("\nJrn.Command \"Ribbon\" , \"Open an existing project , ID_REVIT_FILE_OPEN\" \n");
+            sb.Append("Jrn.Data \"File Name\"  _\n");
+            sb.Append(" , \"IDOK\", \"");
+            sb.Append(filePath).Append("\"\n");
+            return sb.ToString();
+        }
+
+        String CreateRuntheExternalCommand(String commandId)
+        {
+            /*
+              Jrn.RibbonEvent "TabActivated:Add-Ins"
+              Jrn.RibbonEvent "Execute external command:CustomCtrl_%CustomCtrl_%Add-Ins%      3DIQ      %3DIQ Material Proto:HelloRevitCpp.Prototype"
+              Jrn.Data "APIStringStringMapJournalData"  _
+              , 0 
+             Jrn.Data "Transaction Successful"  _
+                       , "Pattern insert CLR"
+           */
+            commandId = "CustomCtrl_%CustomCtrl_%Add-Ins%      3DIQ      %3DIQ Material Proto:HelloRevitCpp.Prototype\"\n";
+            StringBuilder sb = new StringBuilder();
+            //String fileName = "";
+            sb.Append("\nJrn.RibbonEvent \"TabActivated:Add-Ins\" \n");
+            sb.Append("Jrn.RibbonEvent \"Execute external command:CustomCtrl_%CustomCtrl_%Add-Ins%      3DIQ      %3DIQ Material Proto:HelloRevitCpp.Prototype\"\n");
+            sb.Append(" Jrn.Data \"APIStringStringMapJournalData\"  _\n, 0").Append("\n\n");
+            return sb.ToString();
+        }
+
+        String CreateRvtSaveAsCommand(String path)
+        {
+            /*
+                 Jrn.Command "Ribbon" , "Save the active project with a new name , ID_REVIT_FILE_SAVE_AS"
+                 Jrn.Data "File Name"  _
+                 , "IDOK", "C:\Users\Umesh Kumar\Desktop\new\Test case for planner.rvt"
+           */
+          
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\nJrn.Command \"Ribbon\" , \"Save the active project with a new name , ID_REVIT_FILE_SAVE_AS\" \n");
+            sb.Append("Jrn.Data \"File Name\"  _\n");
+            sb.Append(" , \"IDOK\", \"");
+            sb.Append(path).Append("\"\n");
+            return sb.ToString();
+        }
+        String CreateRvtSaveCommand()
+        {
+            /*
+          
+                        Jrn.Command "AccelKey" , "Save the active project , ID_REVIT_FILE_SAVE"
+             */
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\n  Jrn.Command \"AccelKey\" , \"Save the active project , ID_REVIT_FILE_SAVE\" \n\n");
+        
+            return sb.ToString();
+        }
+
+        String CreateRvtCloseDocumentCommand(String fileName, String view)
+        {
+            /*
+               Jrn.Close "[Test case for planner.rvt]" , "3D View: {3D}"
+           */
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\nJrn.Close \"[");
+            sb.Append(fileName);
+            sb.Append("]\" , ");
+            sb.Append("\"").Append(view).Append("\"").Append("\n\n");
+            sb.Append("'*********************************************************************************\n");
+            return sb.ToString();
+        }
+
+        String CreateRvtCloseCommand()
+        {
+            /*
+               
+                Jrn.Command "SystemMenu" , "Quit the application; prompts to save projects , ID_APP_EXIT"
+
+           */
+            StringBuilder sb = new StringBuilder();
+            sb.Append("\nJrn.Command \"SystemMenu\" , \"Quit the application; prompts to save projects , ID_APP_EXIT\"\n");
+            return sb.ToString();
+        }
+        
+    }
+}
---
+++ JournalFileCreation/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JournalFileCreation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String value = Environment.GetEnvironmentVariable("3DIQBatchMode");
+           // value = value.Remove(0, 1);
+          //  value = value.Remove(value.Length-1, 1);
+            if (value != null)
+            {
+
+
+                List<String> list = new List<string>();
+                using (StreamReader reader = new StreamReader(value))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        // Do something with the line.
+                        list.Add(line);
+                        
+                    }
+                }
+                 if (list.Count < 7)
+                    return;
+
+                 String path = list.ElementAt(0);
+                 String addInPath = list.ElementAt(1);
+                 String outPath = list.ElementAt(2);
+                 String revitExePath = list.ElementAt(3);
+                 String idealPatPath = list.ElementAt(4);
+                 String toTestPatPath = list.ElementAt(5);
+                 String roundOffSettings = list.ElementAt(6);
+                 String PateComparisonExe = list.ElementAt(7);
+                 String resultFileName = list.ElementAt(8);
+                 //Example.txt";
+                 
+                 String timeStamp = "Revit"+string.Format("{0:yyyy_MM_dd_hh_mm_ss_tt}", DateTime.Now);
+                 String timeStampoutPath = outPath + timeStamp+@"\";
+                 DirectoryInfo info = Directory.CreateDirectory(timeStampoutPath);
+                 using (var writer = new StreamWriter(value))
+                 {
+                     writer.WriteLine(path);
+                     writer.WriteLine(addInPath);
+                     writer.WriteLine(outPath);
+                     writer.WriteLine(revitExePath);
+                     writer.WriteLine(idealPatPath);
+                     writer.WriteLine(toTestPatPath);
+                     writer.WriteLine(roundOffSettings );
+                     writer.WriteLine(PateComparisonExe);
+                     writer.WriteLine(resultFileName);
+                     writer.WriteLine(timeStampoutPath);
+                 }
+
+                 
+                 string[] filesList = Directory.GetFiles(path);
+                 JournalCommand jnl = new JournalCommand(filesList, timeStampoutPath);
+                 List<String> genralFile  = jnl.GetCommand();
+                 using (var writer = new StreamWriter(addInPath+"Example.txt"))
+                 {
+                   foreach (String item in genralFile)
+                   {
+                     writer.WriteLine(item);
+                   }
+                 }
+                 Process process = Process.Start(revitExePath, "  \"" + addInPath + "Example.txt"+ "\"");
+                 process.WaitForExit();
+
+                 //string command1 //= @"D:\BatchModPatFileComp\PatFileComparison\PatFileComparison\bin\Debug\PatFileComparison.exe";
+                 var process2 = Process.Start(PateComparisonExe, idealPatPath + " " + toTestPatPath + " " + roundOffSettings);
+                 process2.WaitForExit();
+
+                 Process process1 = Process.Start(revitExePath, "  \"" + addInPath + "Reflect.txt" + "\"");
+                 process1.WaitForExit();
+            }
+        }
+
+   
+    }
+}
